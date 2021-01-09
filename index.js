249c19867179f071ac01df8858341a9990a1b933
souvik <souvik2805@gmail.com>
2021-01-09 23:15:31 +0530

    config for herocu

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const bcrypt = require("bcryptjs");
-const port = 8000;
+const port = process.env.PORT || 7000;
 app.use(express.json());
 const { user_Collection, data_Collection } = require("./connector");
 const bodyParser = require("body-parser");
@@ -9,6 +9,26 @@ var cors = require("cors");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
+
+// --> Add this
+// ** MIDDLEWARE ** //
+const whitelist = [
+  "http://localhost:3000",
+  "http://localhost:8080",
+  "https://shrouded-journey-38552.herokuapp.com",
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin);
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable");
+      callback(null, true);
+    } else {
+      console.log("Origin rejected");
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
 
 app.post("/api/create", (req, res) => {
   const { imgURL, description, title } = req.body;
@@ -101,5 +121,15 @@ app.delete("/api/delete/:id", (req, res) => {
     })
     .catch((err) => res.status(404).json({ msg: err.msg }));
 });
+
+// --> Add this
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/build")));
+  // Handle React routing, return all requests to React app
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
 
 app.listen(port, () => console.log(`App listen on port ${port}`));
